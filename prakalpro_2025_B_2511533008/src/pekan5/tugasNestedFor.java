678defcfa7b1a7904e6888688e4463e392af3338
Ghinada Fathanawafa Algma <ghinadafathanawafaalgma@gmail.com>
2025-10-28 19:44:21 +0700

    Add files via upload

--- prakalpro_2025_B_2511533008/src/pekan5/tugasNestedFor.java
+++ prakalpro_2025_B_2511533008/src/pekan5/tugasNestedFor.java
@@ -3,14 +3,47 @@ package pekan5;
 public class tugasNestedFor {
 
 	public static void main(String[] args) {
-		for (int i=1; i<=5; i++) {
-			for (int j=1; j<=(-1*i+5);j++) {
-				System.out.print(".");
-			}
-			
-			System.out.println();
-		}
+	    int[] titik = {0, 4, 8, 12, 12, 8, 4, 0};
+	    int lebarMaks = 12; 
+	    int totalLebarIsi = lebarMaks + 4; 
+		int lebarFrame = totalLebarIsi + 8; 
+		
+		        System.out.print("#");
+		        for (int i = 0; i < lebarFrame; i++) {
+		            System.out.print("=");
+		        }
+		        System.out.println("#");
+   
+		        for (int i = 0; i < titik.length; i++) {
+		            System.out.print("|");
 
+		            int spasi = (lebarFrame - (titik[i] + 4)) / 2;
+		            for (int s = 0; s < spasi; s++) {
+		                System.out.print(" ");
+		            }
+
+		            if (titik[i] == 0) {
+		                System.out.print("<><>");
+		            } else {
+		                System.out.print("<>");
+		                for (int j = 0; j < titik[i]; j++) {
+		                    System.out.print(".");
+		                }
+		                System.out.print("<>");
+		            }
+
+		            for (int s = 0; s < spasi; s++) {
+		                System.out.print(" ");
+		            }
+
+		            System.out.println("|");
+		        }
+
+		        System.out.print("#");
+		        for (int i = 0; i < lebarFrame; i++) {
+		            System.out.print("=");
+		        }
+		        System.out.println("#");
+		    }
 	}
 
-}
