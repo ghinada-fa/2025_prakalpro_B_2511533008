2e736a784e0daaaef1d396d6ba32660b769370fc
Ghinada Fathanawafa Algma <ghinadafathanawafaalgma@gmail.com>
2025-11-10 11:37:59 +0700

    Add files via upload

---
+++ prakalpro_2025_B_2511533008/src/pekan7_2511533008/PanggilMahasiswa_2511533008.java
@@ -0,0 +1,15 @@
+package pekan7_2511533008;
+
+public class PanggilMahasiswa_2511533008 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511533008 a = new Mahasiswa_2511533008();
+		a.setNim(3008);
+		a.setNama("Nadaa");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+
+	}
+
+}
---
+++ prakalpro_2025_B_2511533008/src/pekan7_2511533008/String1_2511533008.java
@@ -0,0 +1,14 @@
+package pekan7_2511533008;
+
+public class String1_2511533008 {
+
+	public static void main(String[] args) {
+		String salam = "Assalamualaikum";
+		System.out.println("Panjang salam adalah: " + salam.length());
+		System.out.println(salam.toUpperCase()); // outputs "ASSALAMUALAIKUM"
+		System.out.println(salam.toLowerCase()); // outputs "assalamualaikum"
+		System.out.println(salam.indexOf("salam"));
+		
+	}
+
+}
